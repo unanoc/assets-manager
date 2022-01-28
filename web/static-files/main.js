25efdb27513a80371e172f4a6cd25debdcda5030
catenocrypt <13562139+catenocrypt@users.noreply.github.com>
2022-01-28 09:31:41 +0100

    Revert "Add Bootstrap stylesheet (#28)"
    This reverts commit 029034a059eec2a3f052999cef3ac95f721879c8.

--- web/static-files/main.js
+++ web/static-files/main.js
@@ -468,7 +468,7 @@ function start() {
         template:
             `
                 <div>
-                    <table class="left">
+                    <table class="center">
                         <tr>
                             <td style="background-color: #E0E0E0;">
                                 <logo-column-preview :logourl="logourl" :logostream="logostream" :tokenname="tokenname" textcolor="black" />
@@ -540,11 +540,11 @@ function start() {
             },
         },
         template: `
-            <div class="col-12 col-lg-7 mt-2">
-                    <h4 class="font-weight-bold">Token preview</h4>
-                    <div class="mt-2 mb-2"><logo-preview :logourl="tokenInfo.logoUrl" :logostream="tokenInfo.logoStream"/></div>
-                    <div class="table-responsive-sm">
-                <table class="table table-borderless">
+            <div id="token-view">
+                <div class="margined">
+                    <h3>Token View</h3>
+                </div>
+                <table>
                     <tr>
                         <td>Type:</td>
                         <td><strong>{{tokenInfo.type}}</strong></td>
@@ -561,10 +561,13 @@ function start() {
                     </tr>
                     <tr>
                         <td colspan="2">
-                        <div class="form p-0 mt-2 mb-2">
-  <textarea class="form-control" placeholder="" id="info.info" readonly="true" rows="8">{{tokenInfo.infoString}}</textarea>
-                      </div>
-
+                            <div>Logo Preview:</div>
+                            <logo-preview :logourl="tokenInfo.logoUrl" :logostream="tokenInfo.logoStream" />
+                        </td>
+                    </tr>
+                    <tr>
+                        <td colspan="2">
+                            <textarea class="input wide" id="info.info" readonly="true" rows="8">{{tokenInfo.infoString}}</textarea>
                         </td>
                     </tr>
                     <tr>
@@ -576,10 +579,12 @@ function start() {
                         <td>Circulation (holders):</td>
                         <td>{{circulationHolders}}</td>
                     </tr>
-
-                              </div>
+                    <tr>
+                        <td colspan="2">
+                            <button class="button" type="button" v-on:click="checkInfoButton">Check</button>
+                        </td>
+                    </tr>
                 </table>
-                    <button class="btn btn-outline-primary" type="button" v-on:click="checkInfoButton">Check</button>
             </div>
         `
     });
@@ -825,11 +830,11 @@ function start() {
         template:
             `
                 <div id="token-search">
-                <div class="row align-items-center pb-2">
-                        <div class="d-inline-flex"><input id="search-input" class="form-control m-1" placeholder="Token" v-on:change="searchButton" v-model="queryString">
-                        <button type="submit" class="btn btn-primary m-1" v-on:click="searchButton">Search</button></div>
-                </div>
-                    <div id="token-search-list" class="c">
+                    <div class="flexrow">
+                        <input id="search-input" class="input wide" placeholder="Token" v-on:change="searchButton" v-model="queryString">
+                        <button v-on:click="searchButton">Search</button>
+                    </div>
+                    <div id="token-search-list" class="selection-list margined">
                         <token-search-item v-for="token in tokens" :token="token" v-on:select="onselecttoken"
                             v-bind:class="{'selected-item': token && selected && token.address == selected.address}"></token-search-item>
                     </div>
@@ -926,12 +931,12 @@ function start() {
             `
                 <tr>
                     <td>Links:</td>
-                    <td class="d-flex align-items-center">
+                    <td>
                         <select class="input" v-model="selectedType">
                             <option v-for="linkType in linkTypes">{{linkType}}</option>
                         </select>
-                        <button class="btn btn-outline-primary btn-sm" type="button" v-on:click="addLink()">Add Link</button>
-                        <span class="smallfont m-2">Add all that apply</span>
+                        <button class="button" type="button" v-on:click="addLink()">Add Link</button>
+                        <span class="smallfont">Add all that apply</span>
                     </td>
                 </tr>
             `
@@ -980,7 +985,7 @@ function start() {
                 }
                 return t;
             },
-            textToArray: function(text) {
+            textToArray: function (text) {
                 text = text.replace(',', '');
                 let arr = text.split(' ');
                 return arr.filter(t => (t.length > 0)); // filter empty ones
@@ -1346,38 +1351,42 @@ function start() {
         },
         template:
             `
-                <div id="main-add-token" class="row mt-3">
+                <div id="main-add-token" class="flexrow">
                     <div id="add-input">
-                        <div class="">
-                            <h4>Add New Token</h4>
-                            <p id="add-explanation" class="smallfont">
-                                    See guide on <a href="https://developer.trustwallet.com/assets/new-asset"
+                        <div class="margined">
+                            <div class="mainheader">Add New Token</div>
+                            <div id="add-explanation" class="smallfont margined">
+                                <div>
+                                    See guide on
+                                    <a href="https://developer.trustwallet.com/assets/new-asset"
                                         target="_blank">developer</a> and
                                     <a href="https://community.trustwallet.com/t/how-to-submit-a-token-logo/3863"
                                         target="_blank">community</a> sites.
-
+                                </div>
+                                <div>
                                     Fill in the token/project details.
                                     If all is OK, a new
                                     <a href="https://docs.github.com/en/free-pro-team@latest/desktop/contributing-and-collaborating-using-github-desktop/creating-an-issue-or-pull-request"
                                         target="_blank">pull request</a> will be created in your name,
                                     against the <a :href="mainRepoUrl"
                                         target="_blank">assets repo</a>.
-                            </p>
+                                </div>
+                            </div>
                         </div>
                         <div>
                             <div>
                                 <logo-preview :logostream="tokenInput.logoStream" v-show="tokenInput.logoStream"
                                     :tokenname="tokenInput.name.substring(0, 16)" />
                             </div>
-                            <div class="mt-3">
-                                <button class="btn btn-outline-primary"
+                            <div>
+                                <button class="button"
                                     onclick="document.getElementById('input.file-selector').click();">Upload Logo</button>
                                 <input id="input.file-selector" type="file" style="display: none;"
                                     v-on:change="logoFileSelected()" />
                                 <span v-html="inputLogoFilename" v-bind:title="inputLogoDetails" id="input.logo-input" class="smallfont"></span>
                             </div>
                             <div class="smallfont error padded">
-                                {{errorLogo}}
+                                {{errorLogo}} 
                             </div>
                             <table class="wide">
                                 <tr>
@@ -1416,7 +1425,7 @@ function start() {
                                         </div>
                                         <div class="smallfont error padded">
                                             <span v-show="fixedContract"><a v-on:click="tokenInput.contract = fixedContract; tokenInputContractChanged()">Fix</a></span>
-                                            {{errorContract}}
+                                            {{errorContract}} 
                                         </div>
                                     </td>
                                 </tr>
@@ -1449,7 +1458,7 @@ function start() {
                                             v-on:change="tokenInputWebsiteChanged()" />
                                         <div class="smallfont error padded">
                                             <span v-show="fixedWebsite"><a v-on:click="tokenInput.website = fixedWebsite; tokenInputWebsiteChanged()">Fix</a></span>
-                                            {{errorWebsite}}
+                                            {{errorWebsite}} 
                                         </div>
                                     </td>
                                 </tr>
@@ -1461,7 +1470,7 @@ function start() {
                                             size="40" v-on:change="tokenInputExplorerChanged()" />
                                         <div class="smallfont error padded">
                                             <span v-show="fixedExplorer"><a v-on:click="tokenInput.explorerUrl = fixedExplorer; tokenInputExplorerChanged()">Fix</a></span>
-                                            {{errorExplorer}}
+                                            {{errorExplorer}} 
                                         </div>
                                     </td>
                                 </tr>
@@ -1479,15 +1488,15 @@ function start() {
                                 <tags-add :tokenInput="tokenInput" />
                             </table>
 
-                            <div class="d-flex justify-content-end">
-                                <button class="btn btn-primary m-1" type="button"
+                            <div>
+                                <button class="button" type="button"
                                     v-on:click="checkInputButton()">
                                     {{checkButtonText ? checkButtonText : 'Check'}}
                                 </button>
-                                <button class="btn btn-outline-primary m-1" type="button" v-on:click="createPullButton()">
+                                <button class="button" type="button" v-on:click="createPullButton()">
                                     {{prButtonText ? prButtonText : 'Create Pull Request'}}
                                 </button>
-                                <button class="btn btn-outline-primary" type="button" v-show="debugMode"
+                                <button class="button" type="button" v-show="debugMode"
                                     v-on:click="clearInput()">Clear</button>
                             </div>
                         </div>
@@ -1495,7 +1504,7 @@ function start() {
                     <div v-show="debugMode" id="add-debug-mode">
                         <div>Debug Mode</div>
                         <div>
-                            <button class="btn btn-outline-primary" type="button" v-on:click="debugFillWithDummyData();">Fill
+                            <button class="button" type="button" v-on:click="debugFillWithDummyData();">Fill
                                 test
                                 data</button>
                             <input type="checkbox" id="debug_pr_target_fork_cb" v-model="debugPrTargetFork" /><label
@@ -1566,9 +1575,9 @@ function start() {
         },
         template:
             `
-                <div class="row justify-content-center ">
-                    <div id="search" class="col-12 col-lg-5 mt-2">
-                        <h4>Token Search</h4>
+                <div class="tab-pane flexrow">
+                    <div id="search">
+                        <div class="mainheader">Token Search</div>
                         <token-search v-on:selecttoken="onSearchSelectToken"></token-search>
                     </div>
                     <token-view :token-info="tokenInfo"></token-view>
@@ -1579,14 +1588,12 @@ function start() {
     Vue.component('warning-no-ghuser', {
         template:
             `
-            <div class="row mt-6">
-                <div class="col-12">
-                    <h3 class="font-weight-bold text-center">Not logged in</h3>
-                    <p class="text-center text-gray-400">
+                <div id="placeholder-no-ghuser" class="main center">
+                    <div class="mainheader">Not logged in</div>
+                    <div class="center">
                         You need to log in with your GitHub account, and authorize this application.
-                    </p>
-                </div>
-             </div>
+                    </div>
+                </div>            
             `
     });
 
